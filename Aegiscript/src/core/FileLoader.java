9638cf27a6fcfab83c8554b3a66ba915da870f8b
prith46 <prithiviraj17a@gmail.com>
2025-06-17 19:51:45 +0530

    Including variables

--- Aegiscript/src/core/FileLoader.java
+++ Aegiscript/src/core/FileLoader.java
@@ -3,14 +3,54 @@ package core;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class FileLoader {
 
+    public static HashMap<String, String> stringVariables = new HashMap<>();
+
+    public static void variables(String line){
+        int start = line.indexOf(" ");
+        int end = line.indexOf("=") - 1;
+        String variableName = line.substring(start + 1, end);
+
+        start = line.indexOf("\"") + 1;
+        end = line.lastIndexOf("\"");
+
+        stringVariables.put(variableName, line.substring(start, end));
+    }
+
     public static void print(String line){
-        StringBuilder str = new StringBuilder();
-        int start = line.indexOf("\"");
-        int end = line.lastIndexOf("\"");
-        System.out.println(line.substring(start + 1, end));
+
+        // Printing with variable
+        if (line.contains("+")){
+            int start = line.indexOf("\"");
+            int end = line.lastIndexOf("\"");
+            System.out.print(line.substring(start + 1, end));
+
+            String variableName;
+            start = line.indexOf("+");
+            end = line.indexOf(")");
+            variableName = line.substring(start + 1, end).trim();
+            System.out.println(stringVariables.getOrDefault(variableName, "Variable not found"));
+            return;
+        }
+
+        // Just a normal printing
+        if (line.contains("\"")) {
+            int start = line.indexOf("\"");
+            int end = line.lastIndexOf("\"");
+            System.out.println(line.substring(start + 1, end));
+            return;
+        }
+
+        // Printing variables
+        String variableName;
+        int start = line.indexOf("(") + 1;
+        int end = line.indexOf(")") - 1;
+        variableName = line.substring(start, end);
+        System.out.println(stringVariables.getOrDefault(variableName, "Variable not found"));
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,6 +59,9 @@ public class FileLoader {
         while ((line = reader.readLine()) != null){
             if (line.startsWith("print"))
                 print(line);
+
+            if (line.startsWith("let"))
+                variables(line);
         }
     }
 }
